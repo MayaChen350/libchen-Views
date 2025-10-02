7cb26d9b9a52e23df21b04950046218e05bf0d4d
Maya <145165822+MayaChen350@users.noreply.github.com>
2025-10-02 01:06:47 -0400

    RecyclerView helper

--- src/main/kotlin/io/github/mayachen350/libchenviews/HasRecyclerView.kt
+++ src/main/kotlin/io/github/mayachen350/libchenviews/HasRecyclerView.kt
@@ -3,8 +3,12 @@ package io.github.mayachen350.libchenviews
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
-interface HasRecyclerView<T,VH : RecyclerView.ViewHolder> {
+/** An interface acting as a contract to an activity to make it promise to have all
+ * the necessary things for any recycler view helper class to be working.
+ *
+ * @see RecyclerViewHelper**/
+open interface HasRecyclerView<T, VH : RecyclerView.ViewHolder> {
     var recyclerView: RecyclerView
 
-    var listAdapter:  ListAdapter<T,VH>
+    var listAdapter: ListAdapter<T, VH>
 }
--- src/main/kotlin/io/github/mayachen350/libchenviews/RecyclerViewHelper.kt
+++ src/main/kotlin/io/github/mayachen350/libchenviews/RecyclerViewHelper.kt
@@ -1,61 +1,20 @@
 package io.github.mayachen350.libchenviews
 
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import ca.cegepmontpetit.cem6222816.tp3_4n6.backend.RetrofitUtil
-import io.github.mayachen350.libchenviews.utils.DefaultCallback
-import retrofit2.Call
-import retrofit2.Response
 
-abstract class RecyclerViewHelper<T>(var view: HasRecyclerView<T, TaskAdapter.TaskViewHolder>) {
+/**
+ * A helper class to be used in an activity inheriting the `HasRecyclerView` interface.
+ *
+ * I strongly suggest using the parameters promised by `HasRecyclerView` when defining your RecyclerViewHelper class.
+ *
+ * @param view Put `This`.
+ * @see HasRecyclerView
+ */
+abstract class RecyclerViewHelper<T>(var view: HasRecyclerView<T, RecyclerView.ViewHolder>) {
 
-    open fun setupRecycler() {
-        view.recyclerView.apply {
-            adapter = view.listAdapter
-            setHasFixedSize(true)
-            addItemDecoration(
-                DividerItemDecoration(
-                    view.recyclerView.context, DividerItemDecoration.VERTICAL
-                )
-            )
+    /** Setup the recycler view. **/
+    abstract fun setupRecycler()
 
-            val itemTouchHelper =
-                ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
-                    ItemTouchHelper.RIGHT,
-                    ItemTouchHelper.LEFT
-                ) {
-                    override fun onMove(
-                        recyclerView: RecyclerView,
-                        viewHolder: RecyclerView.ViewHolder,
-                        target: RecyclerView.ViewHolder
-                    ): Boolean = false
-
-                    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                        val position: Int = viewHolder.adapterPosition
-                        val taskId = (view.listAdapter as TaskAdapter).getTaskId(position)
-                        RetrofitUtil.get().deleteTask(taskId).enqueue( object :
-                            DefaultCallback<String>() {
-                            override fun onSuccessfulResponse(
-                                call: Call<String>,
-                                response: Response<String>
-                            ) {
-                                super.onSuccessfulResponse(call, response)
-                                (adapter as ListAdapter<T, *>).apply {
-                                    val newList = currentList.filterIndexed { index, _ ->
-                                        index != position }
-                                    submitList(newList)
-                                    notifyItemRemoved(position)
-                                }
-                            }
-                        })
-                    }
-                })
-
-            itemTouchHelper.attachToRecyclerView(this)
-        }
-    }
-
+    /** Fill the recycler view with items. **/
     abstract fun fillRecycler()
 }
